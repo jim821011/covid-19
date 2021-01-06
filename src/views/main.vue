2a01f90e3edfe0dbf36ca1eea252bdb04368e0a0
jim821011 <q821011@gmail.com>
2021-01-06 22:43:00 +0800

    creat vue cli

---
+++ src/views/main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ massage }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      massage: 878,
+      covidData: [],
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      const vm = this;
+      const api = 'https://api.covid19api.com/summary';
+      vm.$http.get(api)
+        .then((res) => {
+          console.log(res);
+          vm.covidData = res.data.Countries;
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
